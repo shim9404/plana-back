2ffb95a7e4244f2c235274ea4b1e2fa0d54d98e8
shim yujin <shim9404@gmail.com>
2026-03-24 17:10:50 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/example/plana/PlanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.plana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plana/PlanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
