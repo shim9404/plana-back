841b24c6d7dc498e2218157c8c879eabc68b3d30
shim yujin <shim9404@gmail.com>
2026-03-30 15:03:05 +0900

    remove: 페이지 단위 controller 삭제
    페이지를 기준으로 만들었던 임시 controller 삭제

--- src/main/java/com/example/plana/controller/PlanController.java
+++
@@ -1,9 +0,0 @@
-package com.example.plana.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/plan")
-public class PlanController {
-}
