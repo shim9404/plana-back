46051ffee492070a789bbf0e6b9e2922f72d0f76
hyojin777 <54x9mp@gmail.com>
2026-03-31 09:43:20 +0900

    feat: 닉네임 중복 체크 기능 추가
    닉네임 중복 체크 기능 추가
    닉네임(nickname)을 입력 받아 true 반환 시, 중복이며 false 반환 시, 중복이 아님
    + 모든 경로 허용 가능(securityconfig)
    
    Feat #DEV-43

---
+++ src/main/java/com/example/plana/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.example.plana.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.plana.service.MemberService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/members/")
+public class MemberController {
+    private final MemberService memberService;
+
+    /** DEV-43
+     * dupliNickname(): 닉네임 중복 체크 호출 함수
+     *  -> existNickname(): 닉네임 중복 체크
+     * @param nickname        // 새 닉네임
+     * @return nicknameDupli  // 닉네임 중복 결과(TRUE: 중복 O/ FALSE: 중복 X)
+     */
+    @GetMapping("nickname/check")
+    public boolean dupliNickname(@RequestParam("nickname") String nickname) {
+        boolean nicknameDupli = memberService.existNickname(nickname);
+        return nicknameDupli;
+    }
+
+}
---
+++ src/main/java/com/example/plana/mapper/MemberMapper.java
@@ -0,0 +1,10 @@
+package com.example.plana.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberMapper {
+    // 닉네임 중복 체크
+    boolean existNickname(@Param("nickname") String nickname);
+}
