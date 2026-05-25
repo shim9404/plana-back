373beb0f67a7998f3679a9306e76c52a8a81078f
park minhee <parkmimmi824@gmail.com>
2026-05-25 16:50:49 +0900

    feat: Swagger API 문서 설정 추가
    - ScecurityConfig에 Swagger 관련 설정 추가
    - SwaggerConfig 클래스 추가
    - ResponseBody의 Data 필드 제네릭으로 변경
    !실행 전 application.yml에 Swagger 관련 추가된 설정 갱신 필수
    
    Fixes #DEV-274

--- src/main/java/com/example/plana/dto/common/ResponseBody.java
+++ src/main/java/com/example/plana/dto/common/ResponseBody.java
@@ -2,6 +2,7 @@ package com.example.plana.dto.common;
 
 import com.example.plana.common.exception.ErrorCode;
 import com.example.plana.common.response.SuccessCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +12,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResponseBody {
+public class ResponseBody<T> {
+    @Schema(description = "요청 성공 여부")
     private boolean success;
+    @Schema(description = "응답 코드")
     private Integer status;
+    @Schema(description = "응답 메세지 코드")
     private String code;
+    @Schema(description = "응답 메세지 내용")
     private String message;
-    private Object data;
+    private T data;
 
     // 에러 응답을 위한 정적 팩토리 메서드 추가
-    public static ResponseBody error(ErrorCode errorCode, String message) {
-        return ResponseBody.builder()
+    public static <T> ResponseBody<T> error(ErrorCode errorCode, String message) {
+        return ResponseBody.<T>builder()
                 .success(false)
                 .status(errorCode.getStatus())
                 .code(errorCode.getCode())
@@ -30,8 +35,8 @@ public class ResponseBody {
 
 
     // 성공 응답을 위한 메서드
-    public static ResponseBody success(SuccessCode successCode, Object data) {
-        return ResponseBody.builder()
+    public static <T> ResponseBody<T> success(SuccessCode successCode, T data) {
+        return ResponseBody.<T>builder()
                 .success(true)
                 .status(successCode.getStatus())
                 .message(successCode.getMessage())
