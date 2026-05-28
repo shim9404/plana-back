de42c144a70c6c7726f412a0a29279331ba42593
shim9404 <shim9404@gmail.com>
2026-05-28 17:11:27 +0900

    refactor: RegionController 스웨거 코드 작성
    Fixes #DEV-283

--- src/main/java/com/example/plana/dto/region/read/ZdoResponse.java
+++ src/main/java/com/example/plana/dto/region/read/ZdoResponse.java
@@ -1,5 +1,6 @@
 package com.example.plana.dto.region.read;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,11 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Schema(description = "도 단위 응답")
 public class ZdoResponse {
+    @Schema(description = "도 코드", example = "32")
     private int zdoCode;
+    @Schema(description = "도 이름", example = "강원특별자치도")
     private String zdoName;
     private List<SiguResponse> sigus;
 }
