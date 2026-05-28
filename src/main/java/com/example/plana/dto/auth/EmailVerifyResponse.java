c55591792a32d43775c353dec2e4cee64a272371
shim9404 <shim9404@gmail.com>
2026-05-28 20:39:23 +0900

    refactor: AuthenticationController 스웨거 코드 작성
    Fixes #DEV-281

--- src/main/java/com/example/plana/dto/auth/EmailVerifyResponse.java
+++ src/main/java/com/example/plana/dto/auth/EmailVerifyResponse.java
@@ -1,5 +1,6 @@
 package com.example.plana.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +9,10 @@ import lombok.Getter;
  */
 @Getter
 @AllArgsConstructor
+@Schema(description = "이메일 인증 상태 응답")
 public class EmailVerifyResponse {
+    @Schema(description = "인증 요청한 이메일", example = "sample@gmail.com")
     private String email;
+    @Schema(description = "인증 통과 여부", example = "true")
     private boolean verified;
 }
--- src/main/java/com/example/plana/dto/auth/LogoutRequest.java
+++ src/main/java/com/example/plana/dto/auth/LogoutRequest.java
@@ -1,5 +1,6 @@
 package com.example.plana.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -7,12 +8,15 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Schema(description = "로그아웃 요청")
 public class LogoutRequest {
 
     @NotBlank
     @Email
+    @Schema(description = "로그인한 회원의 email", example = "sample@gmail.com")
     private String email;
 
     @NotBlank(message = "accessToken은 필수입니다.")
+    @Schema(description = "로그인한 회원 정보를 담은 accessToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJNMjciLCJyb2xlIjoiTUVNQkVSIiwiaWF0IjoxNzc1ODEwMDg1LCJleHAiOjE3NzU4MTAzODV9.aT8d_pfXS2gCCUCyFGjzK1K-QiutX7sFdr_dPvA3eUt4VN-rxBX4tUPDw4C7aRnOXoG1VonNjWVH3yIBMgJkyQ")
     private String accessToken;
 }
