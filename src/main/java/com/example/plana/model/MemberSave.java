69273b03e66dbc0d91ca4259ccb8912e6585f9b5
SHIM9404\shim9 <shim9404@gmail.com>
2026-06-12 21:46:45 +0900

    [refactor] postgreSQL 마이그레이션
    - xml 전반 수정
    - 시퀀스 문자 사용 방식 차이에 따른 수정
    
    Fixes #DEV-348

--- src/main/java/com/example/plana/model/MemberSave.java
+++ src/main/java/com/example/plana/model/MemberSave.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberSave {
+    private String memberId;
     private String email;
     private String password;
     private String name;
